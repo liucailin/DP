d2e0a312f2efaa8ad2458ddedec265eb54b5475d
liucailin <liu1356@gmail.com>
2014-10-11 13:48:00 +0800

    Factory Method Pattern

---
+++ src/EnemyShipFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Factory Pattern
+ * 1. Factory method
+ * 2. A set of classes have same super class
+ */
+
+// This is a factory thats only job is creating ships
+// By encapsulating ship creation, we only have one
+// place to make modifications
+public class EnemyShipFactory {
+	
+	// This could be used as a static method if we
+		// are willing to give up subclassing it
+		
+		public EnemyShip makeEnemyShip(String newShipType){
+			
+			//EnemyShip newShip = null;
+			
+			if (newShipType.equals("U")){
+				
+				return new UFOEnemyShip();
+				
+			} else 
+			
+			if (newShipType.equals("R")){
+				
+				return new RocketEnemyShip();
+				
+			} else 
+			
+			if (newShipType.equals("B")){
+				
+				return new BigUFOEnemyShip();
+				
+			} else return null;
+			
+		}
+}
